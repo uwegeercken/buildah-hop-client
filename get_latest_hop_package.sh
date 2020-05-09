35ae9019c06acbf8a9dc78aa478e3e9e8e5ec3b2
uwegeercken <uwegeercken@web.de>
2020-05-09 16:40:24 +0200

    zipfile will only be downloaded if not previously processed

--- get_latest_hop_package.sh
+++ get_latest_hop_package.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
-# script to download the maven package xml file, retrieve the version number,
-# construct the full remote zipfile name, download the file and unzip it to
-# the folder where the script resides.
+# script to download the hop package and unzip it to the current folder.
+# steps:
+# - download maven metadata to determine latest version - this will define the folder to download from
+# - download maven metadata from the folder determined above, to detrmine the package version
+# - variable HOP_LATEST_VERSION will be exported
+# - if the same zipfile has not already been downloaded, download and unzip it
+# - remove metadata files and zip file
 #
-# uwe geercken - 2020-05-08
+# uwe geercken - 2020-05-09
 #
 
 script_dir="$(dirname "$(readlink -f "$0")")"
@@ -12,7 +16,7 @@ maven_metadata_xml=latest_maven-metadata.xml
 package_xml=latest_hop_package.xml
 url="https://artifactory.project-hop.org/artifactory/hop-snapshots-local/org/hop/hop-assemblies-client"
 
-echo "[INFO] getting maven metadata from: ${url}"
+#echo "[INFO] getting maven metadata from: ${url}"
 curl -s -o "${maven_metadata_xml}" ${url}/maven-metadata.xml
 
 # find the latest latest package version in the maven metadata file
@@ -23,7 +27,7 @@ maven_metadata_latest_hop_version=$(echo $maven_metadata_latest_hop_version_xml 
 # full url to the package
 full_url=${url}/${maven_metadata_latest_hop_version}
 
-echo "[INFO] getting maven metadata from: ${full_url}"
+#echo "[INFO] getting maven metadata about package from: ${full_url}"
 curl -s -o "${package_xml}" ${full_url}/maven-metadata.xml
 
 # finding the version in the maven xml file
@@ -37,22 +41,35 @@ export HOP_LATEST_VERSION=${version}
 # construct full zip file name
 zipfile_name="hop-assemblies-client-${version}.zip"
 
+#echo "[INFO] latest hop version zip file: ${zipfile_name}"
+
+# check if we had a previous download
+if [ -f latest_download_version.txt ]
+then
+	previous_download=$(cat latest_download_version.txt)
+fi
+
 # download and unzip file if not already done for the latest version
-if [ ! -f "${zipfile_name}" ]
+if [ "${zipfile_name}" != "${previous_download}" ]
 then
 	echo "[INFO] downloading: ${zipfile_name}"
 	curl -s -o ${zipfile_name} ${full_url}/${zipfile_name}
 
-	echo "[INFO] removing existing folder: ${script_dir}/hop"
+	echo "${zipfile_name}" > latest_download_version.txt
+
+	#echo "[INFO] removing existing folder: ${script_dir}/hop"
 	rm -rf "${script_dir}/hop"
 
 	echo "[INFO] unzipping: ${zipfile_name}"
 	unzip -q ${zipfile_name}
+
+	#echo "[INFO] removing downloaded zip file"
+	rm "${zipfile_name}"
 else
-	echo "[INFO] zip file already exists: ${zipfile_name}"
+	echo "[INFO] latest version already download: ${zipfile_name}"
 fi
 
-echo "[INFO] removing maven metadata file"
+#echo "[INFO] removing maven metadata files"
+rm "${maven_metadata_xml}"
 rm "${package_xml}"
-echo "[INFO] removing downloaded zip file"
-rm "${zipfile_name}"
+
